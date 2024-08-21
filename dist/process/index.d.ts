d2fb51bd56dfb86523291d67c6ddad5f7b555bf0
neki-dev <dev@neki.guru>
2024-08-21 13:26:22 +0200

    chore(*): update dist

--- dist/process/index.d.ts
+++ dist/process/index.d.ts
@@ -3,7 +3,7 @@ import type { PathfindingGrid, PathfindingPosition } from '../types';
 export declare class PathfindingProcess {
     private readonly grids;
     private readonly weights;
-    private taskQueue;
+    private queue;
     private timer;
     constructor(loopRate?: number);
     destroy(): void;
@@ -11,8 +11,7 @@ export declare class PathfindingProcess {
     cancelTask(idLayer: string, idTask: number): void;
     addLayer(idLayer: string, grid: PathfindingGrid): void;
     removeLayer(idLayer: string): void;
-    setWeight(idLayer: string, position: PathfindingPosition, value: number): void;
-    resetWeight(idLayer: string, position: PathfindingPosition): void;
+    setWeight(idLayer: string, position: PathfindingPosition, value: number | null): void;
     setWalkable(idLayer: string, position: PathfindingPosition, state: boolean): void;
     private next;
     private getNextDirections;
